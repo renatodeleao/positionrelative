34e914e9618bb3419d5a1706bdca5b4de9e37630
Renato de Leão <renatodeleao@gmail.com>
2019-03-04 11:49:37 +0000

    [js] Remove test functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,1 @@
-import defaultTestFn, { testVar, testFn } from "./utils/test.js";
 
-console.log(testVar);
-console.log(testFn());
-console.log(defaultTestFn());
--- js/utils/test.js
+++
@@ -1,7 +0,0 @@
-export const testVar = "Hello World";
-
-export const testFn = _ => (`Hello World from testFn`);
-
-const defaultTestFn = _ => (`Hello World from defaultTestFn`)
-
-export default defaultTestFn
