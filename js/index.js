beb3f5ea251f28cc91d5e033b22a1dbb1cc91916
Renato de Leão <renatodeleao@gmail.com>
2019-03-04 11:50:21 +0000

    [js] touchevents detection helper
    - based on modernizr and stackoveflow. I didn't include modernizer because they don't have a modularized system yet, and i don't feel like creating a custom build now.
    - Add html classname if truthy for conditional styling.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+// UTILS
+import hasTouchEvents from "./utils/touchevents.js";
+
+// move to some folder sometime
+(function touch() {
+  if(hasTouchEvents()) {
+    document.documentElement.classList.add('touchevents')
+  }
+})();
+
 
